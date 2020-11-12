8e207d536c855bfaa7be25280fa9154a88312fb5
Steven Cabrera Londoño <32532691+stevenstc@users.noreply.github.com>
2020-11-11 21:57:57 -0500

    Update index.js

--- src/components/Contract/index.js
+++ src/components/Contract/index.js
@@ -1,4 +1,4 @@
 
 /// La direccion de su contrato acá ///////////////////////////////////
-const contractAddress = "TV1R4Vqw3eiabXfaYB42YAeadXYSzXwcwt";
+const contractAddress = "TJ4U6Mb5vJcym9eW7k6EkmrVs8qCe1NVrB";
 export default contractAddress;
